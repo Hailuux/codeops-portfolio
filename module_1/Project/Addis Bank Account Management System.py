4cb1f96538189f56b3527e952d68a8db14d074d1
Hailuux <lebhayyle@gmail.com>
2026-08-19 23:08:34 +0300

    refractor with patterns

--- module_1/Project/Addis Bank Account Management System.py
+++ module_1/Project/Addis Bank Account Management System.py
@@ -1,38 +1,68 @@
 # Addis Bank Account Management System
+class BankConfig:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.interest_rate = 0.05
+            cls._instance.overdraft_limit = 1000
+        return cls._instance
+
+class SMSAlert:
+    def update(self,message):
+        print(f'Sms Alert : {message}')
+
+class AuditLog:
+    def update(self,message):
+        print(f'Audit Log : {message}')
+
 class Account:
     def __init__(self, owner, number, balance = 0):
         self.owner = owner
         self.number = number
         self.__balance = balance
+        self.observers = []
 
     @property
     def balance(self):
         return self.__balance
+    def statement(self):
+        print(f' name = {self.owner} \n account number = {self.number} \n balance = {self.__balance}')
+    def subscribe(self, observer):
+        self.observers.append(observer)
+    def _notify(self, message):
+        for observer in self.observers:
+            observer.update(message)
     def deposit(self, amount):
         if amount <= 0:
             raise ValueError("Amount Must be Positive")
         self.__balance += amount
+        self._notify(f'Deposit : {amount} ETB , New balance : {self.balance} ETB')
     def withdraw(self,amount):
         if self.__balance < amount:
             raise ValueError("Insufficient balance")
         self.__balance -=amount
-    def statement(self):
-        print(f' name = {self.owner} \n account number = {self.number} \n balance = {self.__balance}')
+        self._notify(f'Withdraw : {amount} ETB , New balance : {self.balance} ETB')
+
 
 class SavingsAccount(Account):
-    def __init__(self, owner, number, balance = 0, rate =0.05):
+    def __init__(self, owner, number, balance = 0):
         super().__init__(owner, number, balance)
-        self.rate = rate
+        config = BankConfig()
+        self.rate = config.interest_rate
     def add_interest(self):
         self.deposit(self.balance * self.rate)
     def statement(self):
         print("Account Type: Savings Account")
         super().statement()
+   
 
 class CurrentAccount(Account):
     def __init__(self, owner, number, balance = 0, overdraft = 1000):
         super().__init__(owner, number, balance)
-        self.overdraft = overdraft
+        config = BankConfig()
+        self.overdraft = config.overdraft_limit
     def statement(self):
         print("Account Type: Current Account")
         super().statement()
@@ -41,6 +71,26 @@ class CurrentAccount(Account):
             raise ValueError("Overdraft limit exceeded")
         self._Account__balance -= amount
 
-acc = CurrentAccount("Abebe", "1234", 500, overdraft=1000)
-acc.withdraw(1200) 
-acc.statement()
+
+class AccountFactory:
+    @staticmethod
+    def create(kind, owner, number, balance=0):
+        if kind.lower() == "savings":
+            return SavingsAccount(owner, number, balance)
+        elif kind.lower() == "current":
+            return CurrentAccount(owner, number, balance)
+        raise ValueError("Invalid account type")
+
+sms = SMSAlert()
+audit = AuditLog()
+account1 = AccountFactory.create(
+    "savings",
+    "Abebe",
+    1001,
+    5000
+)
+account1.subscribe(sms)
+account1.subscribe(audit)
+account1.deposit(500)
+account1.add_interest()
+account1.statement()
